c45dbbfa36b28da8b84c23b23689a58a7bfd6f98
Laszlo Szoboszlai <szlaci83@gmail.com>
2017-07-07 01:04:47 +0100

    basic angular2 reg form added

--- webapp/regApp/src/app/app.module.ts
+++ webapp/regApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule  } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { routing} from './app.routing';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     routing
   ],
   providers: [],
--- webapp/regApp/src/app/components/registeruser.component.ts
+++ webapp/regApp/src/app/components/registeruser.component.ts
@@ -2,24 +2,56 @@
  * Created by laci on 06/07/2017.
  */
 import { Component } from '@angular/core';
-import { RegisterUserService} from '../services/registeruser.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+//import { RegisterUserService} from '../services/registeruser.service';
 
 @Component({
   selector: 'register',
   templateUrl: './registeruser.component.html',
-  providers: [RegisterUserService]
+  styleUrls: ['./registeruser.component.css']
+ // providers: [RegisterUserService]
 })
 
 
 export class RegisterUserComponent {
-    user: User;
-    private registerUserService: RegisterUserService;
+
+  rForm: FormGroup;
+  user:any;
+
+  reqAlert: string = 'This fiels is required!';
+
+  id: number = 0;
+  userName: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
+  password: string = "";
+  confirmed: boolean = false;
+  roles: string[] = ["USER"];
+
+
+
+    constructor(private fb: FormBuilder){
+      this.rForm = fb.group({
+         'userName': [null, Validators.required],
+         'email': [null, Validators.required],
+         password: [null, Validators.required],
+         'firstName': '',
+         'lastName': ''
+    });
+    }
+
+   // private registerUserService: RegisterUserService;
 
 
 
 
-    registerUser(User){
-      RegisterUserService.registerUser(User);
+    registerUser(user){
+      this.userName = user.userName;
+      this.email = user.email;
+      console.log(this.userName);
+
+     // RegisterUserService.registerUser(User);
     }
 
 }
